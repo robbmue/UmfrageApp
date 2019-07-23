30a8f72182d30cb2fe2013cf2d602737b190870c
Müller, Robin <rmueller@netzlink.com>
2019-07-23 18:05:45 +0200

    build couch databank and ADDED first approach to access it via curl. Not working until now.

--- app/src/main/java/com/example/dsgvobestandsaufnahme/SyncFragment.java
+++ app/src/main/java/com/example/dsgvobestandsaufnahme/SyncFragment.java
@@ -2,18 +2,28 @@ package com.example.dsgvobestandsaufnahme;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SyncFragment extends Fragment {
+
+    private static final String LOG_TAG = SyncFragment.class.getSimpleName();
 
 
     public SyncFragment() {
@@ -25,7 +35,39 @@ public class SyncFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sync, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_sync, container, false);
+        Button syncButton = rootView.findViewById(R.id.button_sync);
+        syncButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sync(view);
+            }
+        });
+        return rootView;
     }
 
+    public void sync(View view) {
+
+        URL romsCouch = null;
+        try {
+            romsCouch = new URL("http://10.10.6.201");
+        } catch (MalformedURLException e) {
+            Log.d(LOG_TAG, "URL nicht korrekt formatiert.");
+            e.printStackTrace();
+        }
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(romsCouch.openStream(), "UTF-8")) {
+        }) {
+            for (String line; (line = reader.readLine())!=null;){
+                Log.d(LOG_TAG, line);
+            }
+        } catch (UnsupportedEncodingException e) {
+            Log.d(LOG_TAG, "Unsopported Encoding");
+            e.printStackTrace();
+        } catch (IOException e) {
+            Log.d(LOG_TAG, "IO Exception");
+            e.printStackTrace();
+        }
+
+    }
 }
