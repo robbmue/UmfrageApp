a06bcb829f92024645e218dd3421a09776a4adb3
Müller, Robin <rmueller@netzlink.com>
2019-09-03 17:42:15 +0200

    SMALL CHANGES

--- app/src/main/java/com/example/dsgvobestandsaufnahme/OpenFragment.java
+++ app/src/main/java/com/example/dsgvobestandsaufnahme/OpenFragment.java
@@ -42,9 +42,25 @@ public class OpenFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        this.inflater = inflater;
-        return inflater.inflate(R.layout.fragment_open, container, false);
+        View v = inflater.inflate(R.layout.fragment_open, container, false);
+
+        displayOpen = v.findViewById(R.id.displayOpen);
+        displayOpen.setLayoutManager(new LinearLayoutManager(getActivity()));
+        answerList = new ArrayList<>();
+        answerAdapter = new AnswersAdapter(getActivity());
+        displayOpen.setAdapter(answerAdapter);
+        answersViewModel = ViewModelProviders.of(this).get(AnswersViewModel.class);
+        answerList = answersViewModel.getAllAnsweres().getValue();
+        answersViewModel.getAllAnsweres().observe(this, new Observer<List<Answers>>() {
+            @Override
+            public void onChanged(List<Answers> answers) {
+                answerAdapter.setAnswersData(answerList);
+            }
+        });
+        getAnswers();
+        return v;
     }
 
     public void refresh() {
@@ -65,7 +81,6 @@ public class OpenFragment extends Fragment {
                 answerAdapter.setAnswersData(answerList);
             }
         });
-
 
         getAnswers();
 
